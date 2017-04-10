829f203dc5e749f8ce35810e934c1f5516dd6947
kuro <mhgeoe@gmail.com>
2017-04-10 20:20:30 +0900

    make parameters adaptive

--- include/details/mrw.h
+++ include/details/mrw.h
@@ -1,5 +1,6 @@
 #include "../mrw.h"
 
+#include <algorithm>
 #include <random>
 #include <unordered_map>
 #include <vector>
@@ -27,20 +28,24 @@ ptr_t MRW<T>::Search(const vector<int> &variables,
   s_zero->variables_ = variables;
   ptr_t s = s_zero;
   ++generated_;
-  int h_min = heuristic(variables, goal);
+  int initial_h_min = heuristic(variables, goal);
+  h_min_ = initial_h_min;
   ++evaluated_;
   int counter = 0;
   while (!sas_data::GoalCheck(s->variables_, goal)) {
-    if (counter > max_step_ || table.Find(s->variables_).empty()) {
+    if (counter > max_steps_ || table.Find(s->variables_).empty()) {
       s = s_zero;
       counter = 0;
-      std::cout << "restart" << std::endl;
+      h_min_ = initial_h_min;
+      std::cout << "Restart" << std::endl;
+      PrintNewHeuristicValue(h_min_, s->get_g());
     }
     s = PureRandomWalk(goal, sas_operators, table, s);
     int h = heuristic(s->variables_, goal);
-    if (h < h_min) {
-      h_min = h;
+    if (h < h_min_) {
+      h_min_ = h;
       counter = 0;
+      PrintNewHeuristicValue(h_min_, s->get_g());
     } else {
       ++counter;
     }
@@ -56,14 +61,20 @@ ptr_t MRW<T>::PureRandomWalk(
     ptr_t s) {
   int h_min = -1;
   ptr_t s_min = nullptr;
+  int length_walk = length_walk_;
+  int counter = 0;
+  std::cout << "New length of random walk: " << length_walk << std::endl;
+  double p, ap;
+  std::random_device seed_gen;
+  std::default_random_engine engine(seed_gen());
   for (int i=0; i<num_walk_; ++i) {
     ptr_t s_prime = s;
-    for (int j=0; j<length_walk_; ++j) {
+    for (int j=0; j<length_walk; ++j) {
       auto a_set = table.Find(s_prime->variables_);
       if (a_set.empty()) break;
       ++expanded_;
       std::uniform_int_distribution<> dist(0, a_set.size()-1);
-      int a = a_set[dist(engine_)];
+      int a = a_set[dist(engine)];
       auto child = node::Node::Construct();
       ++generated_;
       child->variables_ = s->variables_;
@@ -80,9 +91,28 @@ ptr_t MRW<T>::PureRandomWalk(
     if (h_min == -1 || h < h_min) {
       s_min = s_prime;
       h_min = h;
-      PrintNewHeuristicValue(h_min, s_min->get_g());
+      counter = 0;
+    } else {
+      ++counter;
+      if (counter > extending_period_) {
+        length_walk = static_cast<int>(
+            extending_rate_ * static_cast<double>(length_walk));
+        counter = 0;
+        std::cout << "New length of random walk: " << length_walk << std::endl;
+      }
     }
+    p = std::max(0.0, static_cast<double>(h_min_ - h_min));
+    if (i == 0)
+      ap = p;
+    if (p > ap) {
+      std::cout << "Exploration stopped " << i+1 << " random walks"
+                << std::endl;
+      return s_min;
+    }
+    ap = (1.0-alpha_)*ap + alpha_*p;
   }
+  std::cout << "Exploration stopped " << num_walk_ << " random walks"
+            << std::endl;
   if (s_min == nullptr)
     return s;
   else
--- include/mrw.h
+++ include/mrw.h
@@ -19,16 +19,17 @@ class MRW {
   static_assert(std::is_base_of<heuristic::HeuristicInterface<T>, T>::value,
                 "T is not extended interface class");
  public:
-  MRW()
-      : evaluated_(0),
+  MRW(double alpha=0.9, int num_walk=2000, int length_walk=10,
+      int extending_period=300, double extending_rate=1.5, int max_steps=7)
+      : alpha_(alpha),
+        num_walk_(num_walk),
+        length_walk_(length_walk),
+        extending_period_(extending_period),
+        extending_rate_(extending_rate),
+        max_steps_(max_steps),
+        evaluated_(0),
         expanded_(0),
-        generated_(0),
-        num_walk_(2000),
-        length_walk_(10) {
-          std::random_device seed_gen;
-          std::default_random_engine engine(seed_gen());
-          engine_ = engine;
-        }
+        generated_(0) {}
 
   ~MRW() {}
 
@@ -68,9 +69,13 @@ class MRW {
 
   T heuristic;
   std::default_random_engine engine_;
-  int max_step_;
+  double alpha_;
+  double extending_rate_;
   int num_walk_;
   int length_walk_;
+  int extending_period_;
+  int max_steps_;
+  int h_min_;
   int evaluated_;
   int expanded_;
   int generated_;
